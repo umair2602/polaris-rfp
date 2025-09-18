ab2171aa62bd686c957860e6a466411d7014a37f
Umair Ali <79967462+umair2602@users.noreply.github.com>
2025-09-18 13:21:55 +0500

    Build fix

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export', 
   swcMinify: true,
   env: {
     API_BASE_URL: process.env.API_BASE_URL || 'http://localhost:8000',
